ca728243cc0af24bd64db287a227a6edfcee1be0
Dima Berry <dimaberry1@gmail.com>
2024-01-23 19:10:58 +0100

    feat: solve calculate the final balance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -318,8 +318,12 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce(
+    (acc, e) =>
+      acc + e.reduce((acc2, e2) => (acc2 === 0 ? acc2 + e2 : acc2 - e2), 0),
+    0
+  );
 }
 
 /**
