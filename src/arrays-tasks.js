6d7eb6c1d17db4144b36d07e2c7396519d8a8228
Dima Berry <dimaberry1@gmail.com>
2024-01-29 00:47:59 +0100

    feat: solve find the length of the longest increasing subsequence of a given array of integers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -517,8 +517,28 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  let prev = nums[0];
+  let acc = 1;
+
+  const arr = [];
+  nums.map((e) => {
+    if (e > prev) {
+      acc += 1;
+      arr.push(acc);
+      prev = e;
+    } else {
+      acc = 1;
+      arr.push(acc);
+      prev = e;
+    }
+    return e;
+  });
+  return Math.max(...arr);
 }
 
 /**
